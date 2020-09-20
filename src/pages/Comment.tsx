802b9406fb2737cb8ea1448e0de780e47d6d6497
realeve <realeve@qq.com>
2020-09-20 11:18:18 +0800

    评论内容显示

--- src/pages/Comment.tsx
+++ src/pages/Comment.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styles from './comment.less';
 
-export default ()=>{
-    return <div className={styles.wrap}>
-        <div className={styles.content}>
-            这里滚动显示用户评论
-        </div>
+const CommentItem = ({ comment = '' }) => (
+  <div className={styles.item}>
+    <div className={styles.title}>我承诺</div>
+    <div className={styles.comment}>{comment}</div>
+  </div>
+);
+
+export default () => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.content}>
+        <CommentItem comment="这里滚动显示用户评论,这里滚动显示用,户评论" />
+        <CommentItem comment="敬畏粮食 尊重劳动" />
+        <CommentItem comment="敬畏粮食 尊重劳动" />
+        <CommentItem comment="敬畏粮食 尊重劳动" />
+        <CommentItem comment="敬畏粮食 尊重劳动" />
+        <CommentItem comment="敬畏粮食 尊重劳动" />
+        <CommentItem comment="敬畏粮食 尊重劳动" /> 
+      </div>
     </div>
-}
+  );
+};
--- src/pages/led.tsx
+++ src/pages/led.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './led.less';
 import Comment from './Comment';
+import classnames from 'classnames';
 
 export default () => {
   return (
     <div className={styles.page}>
       <div className={styles.left}>
         <div className={styles.sloganWrapper}>
-          <div className={styles.text}>光盘行动</div>
+          <div className={classnames(styles.text, styles.border)}>光盘行动</div>
           <div className={styles.text} style={{ marginTop: 140 }}>
             从我做起
           </div>
@@ -16,7 +17,7 @@ export default () => {
       <Comment />
       <div className={styles.right}>
         <div className={styles.sloganWrapper}>
-          <div className={styles.text}>厉行节约</div>
+          <div className={classnames(styles.text, styles.border)}>厉行节约</div>
           <div className={styles.text} style={{ marginTop: 140 }}>
             制止浪费
           </div>
